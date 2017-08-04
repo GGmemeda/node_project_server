712cc23217dc05993184214dd3499b325e8f877a
LAPTOP <caobinhen6@gmail.com>
2017-08-04 18:03:58 +0800

    日志打印

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const debug = require('debug')('server');
-const  {PORT}  = require("./config/const.js");
+const  {PORT}  = require("./handle/const.js");
 const hostname = '127.0.0.1';
 
 //中间件设置
@@ -20,9 +20,9 @@ app.use(bodyParser.json());
 //跨域
 app.use(cors());
 //cookie
-app.use(cookieParser())
-
-
+app.use(cookieParser());
+//日志
+app.use(require('./handle/logger'));
 
 
 //接口文件引入
@@ -30,11 +30,11 @@ require('./api/test.js')(app);
 
 //端口
 app.set('port', process.env.PORT || PORT);
-var port=app.get('port');
-var server = http.listen(port, function () {
+let port=app.get('port');
+let server = http.listen(port, function () {
 
-    var host = server.address().address;
+    let host = server.address().address;
 
-    var port = server.address().port;
+    let port = server.address().port;
    console.log('Example app listening at http://%s:%s', host, port);
-})
+});
